b97074ee531ea2f458441ef01de7c869ebce62cc
hart <noblehart8@gmail.com>
2021-12-19 10:24:16 +0100

    removed unused package from main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,13 @@
 import 'package:flutter/material.dart';
 //import 'package:foodsub/app/app.router.dart';
 import 'package:foodsub/ui/views/home/home_view.dart';
-import 'package:overlay_support/overlay_support.dart';
-import 'package:stacked_services/stacked_services.dart';
-import 'package:stacked_themes/stacked_themes.dart';
+// import 'package:overlay_support/overlay_support.dart';
+// import 'package:stacked_services/stacked_services.dart';
+// import 'package:stacked_themes/stacked_themes.dart';
 //import 'package:stacked_services/stacked_services.dart';
 
 import 'app/app.locator.dart';
-import 'app/app.router.dart';
+// import 'app/app.router.dart';
 
 Future main() async {
   WidgetsFlutterBinding.ensureInitialized();
