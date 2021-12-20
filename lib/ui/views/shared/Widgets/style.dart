3fa2eb84a0ebb71b39358a8507a9a708ab200731
hart <noblehart8@gmail.com>
2021-12-20 19:38:03 +0100

    Created color and style guide

---
+++ lib/ui/views/shared/Widgets/style.dart
@@ -0,0 +1,119 @@
+import'package:flutter/materal.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'color.dart';
+
+
+class AppTextStyles{
+
+  static TextStyle heading1 = GoogleFonts.gilroy(
+    fontWeight:FontWeight.w500,
+    color: AppColor.black,
+    fontsize:28,
+    
+    
+    );
+
+     static TextStyle heading2 = GoogleFonts.gilroy(
+    fontWeight:FontWeight.w500,
+    color: AppColor.black,
+    fontsize:24,
+    
+    
+    );
+
+
+
+
+static TextStyle heading3 = GoogleFonts.gilroy(
+    fontWeight:FontWeight.w500,
+    color: AppColor.black,
+    fontsize:18,
+    
+    
+    );
+
+
+
+static TextStyle subtitle = GoogleFonts.gilroy(
+    fontWeight:FontWeight.w500,
+    color: AppColor.black,
+    fontsize:16,
+    
+    
+    );
+
+    static TextStyle smallSubtitle = GoogleFonts.gilroy(
+    fontWeight:FontWeight.w500,
+    color: AppColor.black,
+    fontsize:14,
+    
+    
+    );
+
+    static TextStyle smallText = GoogleFonts.gilroy(
+    fontWeight:FontWeight.w500,
+    color: AppColor.black,
+    fontsize:12,
+    
+    
+    );
+
+
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
