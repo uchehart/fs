ae87be8cc7946b55d3f727ab7d98e510e7dfe4fa
Mubarak Shuaib <mubarakshuaib3@gmail.com>
2021-12-21 11:25:48 +0100

    update

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 //import 'package:foodsub/app/app.router.dart';
 import 'package:foodsub/ui/views/home/home_view.dart';
-import 'package:overlay_support/overlay_support.dart';
 import 'package:stacked_services/stacked_services.dart';
 import 'package:stacked_themes/stacked_themes.dart';
 //import 'package:stacked_services/stacked_services.dart';
