33128c9e809830e53d19fc2fe84d3b1c0e995150
William Tobi <akeemtobi6@gmail.com>
2021-12-22 11:52:55 -0800

    Appbar and style file refactor

--- lib/ui/views/shared/Widgets/style.dart
+++ lib/ui/views/shared/Widgets/style.dart
@@ -1,119 +1,42 @@
-import'package:flutter/materal.dart';
+import 'package:flutter/material.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:google_fonts/google_fonts.dart';
-import 'color.dart';
-
-
-class AppTextStyles{
-
-  static TextStyle heading1 = GoogleFonts.gilroy(
-    fontWeight:FontWeight.w500,
-    color: AppColor.black,
-    fontsize:28,
-    
-    
-    );
-
-     static TextStyle heading2 = GoogleFonts.gilroy(
-    fontWeight:FontWeight.w500,
-    color: AppColor.black,
-    fontsize:24,
-    
-    
-    );
-
-
-
-
-static TextStyle heading3 = GoogleFonts.gilroy(
-    fontWeight:FontWeight.w500,
-    color: AppColor.black,
-    fontsize:18,
-    
-    
-    );
-
-
-
-static TextStyle subtitle = GoogleFonts.gilroy(
-    fontWeight:FontWeight.w500,
-    color: AppColor.black,
-    fontsize:16,
-    
-    
-    );
-
-    static TextStyle smallSubtitle = GoogleFonts.gilroy(
-    fontWeight:FontWeight.w500,
-    color: AppColor.black,
-    fontsize:14,
-    
-    
-    );
-
-    static TextStyle smallText = GoogleFonts.gilroy(
-    fontWeight:FontWeight.w500,
-    color: AppColor.black,
-    fontsize:12,
-    
-    
-    );
-
-
-    
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+import 'colors.dart';
+
+class AppTextStyles {
+  static TextStyle heading1 = GoogleFonts.montserrat(
+    fontWeight: FontWeight.w500,
+    color: AppColors.black,
+    fontSize: 28,
+  );
+
+  static TextStyle heading2 = GoogleFonts.montserrat(
+    fontWeight: FontWeight.w500,
+    color: AppColors.black,
+    fontSize: 24,
+  );
+
+  static TextStyle heading3 = GoogleFonts.montserrat(
+    fontWeight: FontWeight.w500,
+    color: AppColors.black,
+    fontSize: 18,
+  );
+
+  static TextStyle subtitle = GoogleFonts.montserrat(
+    fontWeight: FontWeight.w500,
+    color: AppColors.black,
+    fontSize: 16,
+  );
+
+  static TextStyle smallSubtitle = GoogleFonts.montserrat(
+    fontWeight: FontWeight.w500,
+    color: AppColors.black,
+    fontSize: 14,
+  );
+
+  static TextStyle smallText = GoogleFonts.montserrat(
+    fontWeight: FontWeight.w500,
+    color: AppColors.black,
+    fontSize: 12,
+  );
 }
